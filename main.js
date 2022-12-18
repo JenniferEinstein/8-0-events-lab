780ed6a21d3f652e02526532a9d127a1fa56b0a6
JenniferEinstein <jennifereinstein@gmail.com>
2022-12-18 14:26:35 -0500

    barely touched

--- main.js
+++ main.js
@@ -7,3 +7,20 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+  // if the color clicked on in the #palette, is not the #current-color, change the element background to the #current-color. But which element?
+
+//And how do you change it? Do you set it? What is the actual function
+  //do i need to do this separately for every color/box?
+
+
+  //
+element.addEventListener("click", function(){
+  element.currentColor === 
+});
+
+
+const clicky = document.querySelector("#palette");
+clicky.addEventListener("click", ()=>{
+  #current-color  #;
+});
+//when you click on a cell, change it to the current color.
