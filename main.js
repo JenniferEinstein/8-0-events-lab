aebba56377e729ba052ff57548225d76e83b60f4
JenniferEinstein <jennifereinstein@gmail.com>
2022-12-18 16:19:27 -0500

    two tests passed; understanding about the same

--- main.js
+++ main.js
@@ -23,14 +23,25 @@ for (let i = 0; i < 100; i++) {
 //div dot color says that it is picking only those divs wich also has the class of color
 const clicky = document.querySelectorAll("div.color");
 const currentShade = document.querySelector("#current-color")
-
+const neo = document.querySelectorAll(".cell");
 
 clicky.forEach((element)=> {
   element.addEventListener("click", (event)=>{
     const newish=element.style.background  //saves background color of what we just clicked on to "newish"
     currentShade.style.background = newish
   });
-})
+});
 
 
 //when you click on a cell, change it to the current color.
+//use .forEach to interate through all the cells
+//listen for the click
+//if it clicks on a cell, change the background color of that cell to the current-color.
+
+neo.forEach((cells)=> {
+  cells.addEventListener("click",(event)=>{
+    cells.style.background = currentShade.style.background;
+    });
+});
+
+//clear the board
