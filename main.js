cc8bcc12942f69825de9afbedfad1ca8670bb551
JenniferEinstein <jennifereinstein@gmail.com>
2023-01-08 21:17:40 -0500

    still trying to clear the board

--- main.js
+++ main.js
@@ -6,29 +6,30 @@ for (let i = 0; i < 100; i++) {
   main.append(div);
 }
 
+//The above code creates the grid of 100 squares. It also creates a way to reference "main". For each of those squares, a DIV is created
+
 // You may write your code here!
-  // if the color clicked on in the #palette, is not the #current-color, change the element background to the #current-color. But which element?
+
+// if the color clicked on in the #palette is not the #current-color, change the element background to the #current-color. But which element?
 
 //And how do you change it? Do you set it? What is the actual function
-  //do i need to do this separately for every color/box?
+//do i need to do this separately for every color/box?
 
 
-  //
 
-
-// element.addEventListener("click", function(){
-//   element.currentColor === 
-// });
-
-//div dot color says that it is picking only those divs wich also has the class of color
+//div dot color says that it is picking only those divs which also have the class of color
 const clicky = document.querySelectorAll("div.color");
-const currentShade = document.querySelector("#current-color")
+const currentShade = document.querySelector("#current-color");
 const neo = document.querySelectorAll(".cell");
+//const Jennifer = document.querySelector("#J");
+
 
 clicky.forEach((element)=> {
   element.addEventListener("click", (event)=>{
-    const newish=element.style.background  //saves background color of what we just clicked on to "newish"
-    currentShade.style.background = newish
+    const newish=element.style.background;  
+      //saves background color of what we just clicked on to "newish"
+    currentShade.style.background = newish;
+      //turns the current shade to whatever we just saved to "newish"
   });
 });
 
@@ -45,3 +46,27 @@ neo.forEach((cells)=> {
 });
 
 //clear the board
+
+//const clearBoard = document.querySelector("#clear-board");
+//should clearBoard be createElement? That is how Jinseok did it.
+const clearBoard = document.createElement("button");
+clearBoard.textContent= "Clear Board";
+
+//need to iterate this. 
+neo.forEach((cells) => {
+  clearBoard.addEventListener("click", (event) => {
+    for (let cell of cells) {
+      cells.style.background = "";  
+  }
+  currentShade.style.background = "";
+})});
+
+
+//didn't realized I needed a new Div.
+const newDiv = document.createElement("div");
+currentShade.after(newDiv);
+newDiv.append(clearBoard); //what does this append to?
+
+
+
+
